4a00c97ea7d8b8e89cc2401c9e5445abaaf62b36
jbulant <jbulant@student.42.fr>
2018-04-30 00:44:10 +0200

    coucou

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jbulant <jbulant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/21 00:37:28 by jbulant           #+#    #+#             */
-/*   Updated: 2018/04/29 19:56:38 by jbulant          ###   ########.fr       */
+/*   Updated: 2018/04/29 23:13:55 by jbulant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,9 @@ void msh_clean(t_minishell *msh)
 	// ft_arstrdel(msh->virtual_env);
 }
 
-int	redirect_eof(void)
-{
-	struct termios old_termios;
-	struct termios new_termios;
-
-	tcgetattr(0,&old_termios);
-	new_termios             = old_termios;
-	new_termios.c_cc[VEOF]  = 3;
-	tcsetattr(0,TCSANOW,&new_termios);
-	return (0);
-}
-
 void sighandler(int sig)
 {
 	signal(sig, SIG_IGN);
-	redirect_eof();
 	ft_putchar('\n');
 	ft_putstr(update_ps(PS_DEFAULT));
 	signal( SIGINT, sighandler );
@@ -53,11 +40,7 @@ int main(int ac, char **av, char **env)
 	signal( SIGINT, sighandler );
 	msh_init(env, &msh);
 	while(msh.status != TERMINATE)
-	{
 		msh.action(&msh);
-		// if (g_sigint == 1)
-		// 	g_sigint = 0;
-	}
 	msh_clean(&msh);
 	return (msh.retval);
 }
